07151e212ce9a5f660d719b1277a4e3e369165bd
Ian10037932 <iankuo10037932@gmail.com>
2025-11-03 13:06:10 +0800

    Correct sum calculation from m to n
    Fix the loop to correctly calculate the sum from m to n.

--- 程式語言/plus.cpp
+++ 程式語言/plus.cpp
@@ -1,1 +1,24 @@
+#include<stdio.h>
+//1.輸入兩個數字m和n,m<=n,計算從m家到n的和 包含m
+int main(){
+	int a,m,n,i,total;
+	int b=0;
 
+	scanf("%d%d",&m,&n);
+
+	if(m>n){
+		a=n;
+		n=m;
+	}
+	else{
+		a=m;
+	}
+
+	for(i=a;i<=m;i++){
+		b=b+i;
+	}
+
+	total=b/(n-a);
+
+	return 0;
+{
